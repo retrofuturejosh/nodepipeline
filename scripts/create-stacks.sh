1310df88c77787a291403c23c1d568a9ba4f6342
retrofuturejosh <joshuadsohn@gmail.com>
2018-07-26 17:38:51 -0400

    [adds] appspec.yml, npm script files, [moves] cloudformation yml to folder

--- scripts/create-stacks.sh
+++ scripts/create-stacks.sh
@@ -2,7 +2,7 @@
 
 #create resources
 echo creating resources
-aws cloudformation create-stack --stack-name node-app-stack --template-body file://resources.yml --capabilities CAPABILITY_IAM --parameters file://parameters.json
+aws cloudformation create-stack --stack-name node-app-stack --template-body file://cloudformation-templates/resources.yml --capabilities CAPABILITY_IAM --parameters file://cloudformation-templates/parameters.json
 
 echo waiting for successful creation of stack
 #wait for resources to be created
@@ -10,7 +10,7 @@ aws cloudformation wait stack-create-complete --stack-name node-app-stack
 
 echo creating pipeline
 #create pipeline
-aws cloudformation create-stack --stack-name node-pipeline-stack --template-body file://codepipeline.yml --capabilities CAPABILITY_IAM --parameters file://parameters.json
+aws cloudformation create-stack --stack-name node-pipeline-stack --template-body file://cloudformation-templates/codepipeline.yml --capabilities CAPABILITY_IAM --parameters file://cloudformation-templates/parameters.json
 
 echo waiting for successful creation of pipeline
 #wait for pipeline to be created
---
+++ scripts/npm-start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /var/nodeapp
+npm start
